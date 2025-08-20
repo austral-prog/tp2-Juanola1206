72d009c5aade643dd1e29b3c52618f8ea0c74209
Juanola1206 <jinores@mail.austral.edu.ar>
2025-08-19 22:08:07 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -1,3 +1,6 @@
 def change():
     expense = 23.75
     money = 100
+    print(f"Ingresar gasto:\n{expense}\nDinero recibido\n{money}\n\nVuelto\n\nPesos:\n{int(money - expense)}\nCentavos:\n{int((money - expense) * 100 % 100)}")
+
+#change()
--- earth.py
+++ earth.py
@@ -1,7 +1,7 @@
-x="Bangladesh"
+def earth():
+    x = "Bangladesh"
+    y = "Barbados"
+    print(f"The result of {x} comes first in the dictionary than {y} is {x < y}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {y < x}.")
 
-y="Barbados"
-
-print("The result of",x,"comes first in the dictionary than",y,"is",x<y)
-
-print("The result of",y,"comes first in the dictionary than",x,"is",y<x)
+#earth()
